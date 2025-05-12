e8e7ed316c5e3df8a8cf79d5f12bdcf191e2435d
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-12 14:07:01 +0300

    add button click me in app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,10 +4,13 @@ import Book from "./Book/Book";
 import Button from "../Button/Button";
 
 export default function App() {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Clicked!", event);
+    console.log("Target:", event.target); // сам <button>
+  };
   return (
     <>
       <h1>Products</h1>
-
       <Product
         name="Tacos With Lime"
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
@@ -18,15 +21,13 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
         price={14.29}
       />
-
       <Mailbox
         username="John"
         messages={["Welcome!", "Don't miss our sale!"]}
       />
-
       <Button variant="primary" text="Login" />
       <Button variant="secondary" text="Follow" />
-
+      <button onClick={handleClick}>Click me!</button>;
       <Book />
     </>
   );
