7b3553fc6699c0d211db1ef13a88d84e42a894da
Andriy Romanov <mgm.agro04@gmail.com>
2025-05-14 17:36:48 +0300

    Request processing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,11 +8,30 @@ import ClickCounter from "../ClickCounter/ClickCounter";
 import ClickCounterTogether from "../ClickCounterTogether/ClickCounterTogether";
 import Several from "../Several/Several";
 import UserMenu from "../UserMenu/UserMenu";
-import OrderForm from "../../OrderForm/OrderForm";
+import OrderForm from "../OrderForm/OrderForm";
+import axios from "axios";
+import SearchForm from "../SearchForm/SearchForm";
+
+interface Article {
+  objectID: string;
+  title: string;
+  url: string;
+}
+
+interface ArticlesHttpResponse {
+  hits: Article[];
+}
 
 export default function App() {
   const handleOrder = (data: string) => {
     console.log("Order reseived from:", data); // можна зберегти замовлення, викликати API, показати повідомлення тощо
+  };
+  const handleSearch = async (topic: string) => {
+    // Виконуємо HTTP-запит
+    const response = await axios.get<ArticlesHttpResponse>(
+      `https://hn.algolia.com/api/v1/search?query=${topic}`
+    );
+    console.log(response.data); // об'єкт з властивістю hits
   };
   /* const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     console.log("Clicked!", event);
@@ -34,6 +53,7 @@ export default function App() {
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?w=640"
         price={10.99}
       />
+      <SearchForm onSubmit={handleSearch} />
       <Product
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?w=640"
